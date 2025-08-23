26d43fd796c43b784e86e542cfa10eec676cccb3
dineshcloudrepo <dineshkumar91cbe@gmail.com>
2025-08-23 22:26:56 +0300

    comments

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -6,6 +6,10 @@ namespace sampleapi.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
+
+        //add comments
+
+
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
